5226cd6eb02d64c3bc4f42e0d77370c7aca1d73b
NyiteX <kitheus09@gmail.com>
2022-10-18 15:02:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.Write("Имя директории (C:/path): ");
+string? dir = Console.ReadLine();
+if (dir == "") dir = "C:\\";
+
+Walk(new DirectoryInfo(dir));
+
+void Walk(DirectoryInfo dir)
+{
+    FileInfo[]? files = null;
+    DirectoryInfo[]? subDirs = null;   
+
+    try
+    {
+        files = dir.GetFiles("*.*");
+        subDirs = dir.GetDirectories();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    if (subDirs != null && files != null)
+    {
+        foreach (var fi in files)
+            Console.WriteLine("     " + fi.FullName);
+
+        foreach (var dirInfo in subDirs)
+        {
+            Console.WriteLine(dirInfo.Name);
+            Walk(dirInfo);
+        }
+    }
+}
